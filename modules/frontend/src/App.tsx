cf4779564d448cacb26cb26eb6f396e60c2e08ea
Ekku Sipilä <ekku.sipila@bonsky.com>
2021-03-15 21:30:21 +0200

    Update Logrocket config to not start session on private user and dev env

--- modules/frontend/src/App.tsx
+++ modules/frontend/src/App.tsx
@@ -4,15 +4,8 @@ import { BrowserRouter } from "react-router-dom";
 import ApolloProvider from "apollo/ApolloProvider";
 import Routes from "routes/Routes";
 import { ThemeProvider } from "styled-components";
-import LogRocket from "logrocket";
 
 import theme from "./theme";
-
-LogRocket.init(
-  process.env.NODE_ENV === "production"
-    ? "6hrsm3/zen-tracking"
-    : "6hrsm3/zen-tracking-dev"
-);
 
 const App = (): JSX.Element => {
   return (
