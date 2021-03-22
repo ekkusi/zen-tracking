0508170339da2c0f294fd9078bd314001a9fbc66
Ekku Sipilä <ekku.sipila@bonsky.com>
2021-03-22 22:04:00 +0200

    Add cors allow for heroku site and also change scrolling refresh to mobile instead of pwa

--- modules/frontend/src/components/ScrollToTop.tsx
+++ modules/frontend/src/components/ScrollToTop.tsx
@@ -1,16 +1,19 @@
 import { useMediaQuery } from "@chakra-ui/react";
 import { useEffect } from "react";
 import { useHistory, useLocation } from "react-router-dom";
+import theme from "../theme";
 
 export default function ScrollToTop() {
   const { pathname } = useLocation();
 
-  const [isDisplayingInPwa] = useMediaQuery("(display-mode: standalone)");
+  const [isDisplayingInMobile] = useMediaQuery(
+    `(max-width: ${theme.breakpoints.sm})`
+  );
 
   const history = useHistory();
 
   window.addEventListener("wheel", (e: WheelEvent) => {
-    if (e.deltaY < -300 && e.pageY < 100 && isDisplayingInPwa) {
+    if (e.deltaY < -300 && e.pageY < 100 && isDisplayingInMobile) {
       console.log(e);
       window.location.href = history.location.pathname;
     }
