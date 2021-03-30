c13011c498a31afc78e60bbcc1aa3b27e6bc6906
Ekku Sipilä <ekku.sipila@bonsky.com>
2021-03-30 10:26:08 +0300

    Only add overscroll refresh listeners when in PWA

--- modules/frontend/src/components/functional/ScrollToTop.tsx
+++ modules/frontend/src/components/functional/ScrollToTop.tsx
@@ -1,3 +1,4 @@
+import { useMediaQuery } from "@chakra-ui/react";
 import { useEffect } from "react";
 import { useHistory, useLocation } from "react-router-dom";
 
@@ -5,33 +6,35 @@ export default function ScrollToTop() {
   const { pathname } = useLocation();
 
   const history = useHistory();
+  const [isDisplayingInPwa] = useMediaQuery("(display-mode: standalone)");
 
-  let _startY: number | null;
+  if (isDisplayingInPwa) {
+    let _startY: number | null;
+    window.addEventListener(
+      "touchstart",
+      (e) => {
+        const documentScrollTop = document?.scrollingElement?.scrollTop || 0;
+        // Only start possible refresh, if touch is happening on top or above page
+        if (documentScrollTop <= 0) {
+          _startY = e.touches[0]?.pageY || 0;
+        } else _startY = null;
+      },
+      { passive: true }
+    );
 
-  window.addEventListener(
-    "touchstart",
-    (e) => {
-      const documentScrollTop = document?.scrollingElement?.scrollTop || 0;
-      // Only start possible refresh, if touch is happening on top or above page
-      if (documentScrollTop <= 0) {
-        _startY = e.touches[0]?.pageY || 0;
-      } else _startY = null;
-    },
-    { passive: true }
-  );
-
-  window.addEventListener(
-    "touchend",
-    (e) => {
-      const y = e.changedTouches[0]?.pageY || 0;
-      const documentScrollTop = document?.scrollingElement?.scrollTop || 0;
-      // Refresh is started on top of the page AND end touch is 200 pixels above the start touch
-      if (_startY && documentScrollTop <= 0 && y > _startY + 150) {
-        window.location.href = history.location.pathname;
-      }
-    },
-    { passive: true }
-  );
+    window.addEventListener(
+      "touchend",
+      (e) => {
+        const y = e.changedTouches[0]?.pageY || 0;
+        const documentScrollTop = document?.scrollingElement?.scrollTop || 0;
+        // Refresh is started on top of the page AND end touch is 200 pixels above the start touch
+        if (_startY && documentScrollTop <= 0 && y > _startY + 150) {
+          window.location.href = history.location.pathname;
+        }
+      },
+      { passive: true }
+    );
+  }
 
   useEffect(() => {
     window.scrollTo(0, 0);
