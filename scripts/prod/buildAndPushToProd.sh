6f93bccf3b6c1464ff10ac0153399094d8459448
Ekku Sipilä <ekku.eki@gmail.com>
2021-04-06 16:42:29 +0300

    Fix buildANdPushToProd script

--- scripts/prod/buildAndPushToProd.sh
+++ scripts/prod/buildAndPushToProd.sh
@@ -5,6 +5,6 @@ parentPath=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd "$parentPath"
 cd "../../"
 
-docker build -t frontend .
+docker build -t zen-tracking .
 docker save zen-tracking | gzip > ./tmp/zen-tracking.tar.gz
 scp ./tmp/zen-tracking.tar.gz eke@136.244.97.223:~/zen-tracking/tmp 
