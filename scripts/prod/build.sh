edfe138a07276a8befa2a5f6ea1cd3478e58704e
Ekku Sipilä <ekku.sipila@bonsky.com>
2021-05-11 12:14:48 +0300

    Remove unnecessaryr script

--- scripts/prod/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-parentPath=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
-cd "$parentPath"
-
-docker-compose -f ../../docker-compose.prod.yml up -d --no-deps --build zen-tracking
-
